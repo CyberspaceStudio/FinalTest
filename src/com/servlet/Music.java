83eedbcab775dba31df486dac0d29b48aa8424ce
zhangyonglele <zhangyonglele@yeah.net>
2018-11-08 22:49:36 +0800

    更新servlet类

--- src/com/servlet/Music.java
+++ src/com/servlet/Music.java
@@ -36,6 +36,6 @@ public class Music extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
